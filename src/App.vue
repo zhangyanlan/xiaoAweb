d37ce3a2c2d81b06eef9cfbea3e3c07d1a2671fd
wwe1056416465 <1056416465@qq.com>
2019-04-04 09:14:58 +0800

    xiaoA移动web项目搭建

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
+
 <script>
 	export default {
 		onLaunch: function () {
 			console.log('App Launch')
+			
 		},
+		
 		onShow: function () {
 			console.log('App Show')
 		},
@@ -13,5 +16,9 @@
 </script>
 
 <style>
+/* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
+	@import './common/uni.css';
 	/*每个页面公共css */
+	@import "./static/base.scss";
+
 </style>
---
+++ src/pages/tabBar/index/index.vue
@@ -0,0 +1,108 @@
+<template>
+	<view class="content">
+		<view class="yh_card">
+			<view class="index-title">数据统计</view>
+			<view class="uni-flex uni-row">
+				<view class="flex-item ">A</view>
+				<view class="flex-item ">B</view>
+				<view class="flex-item ">C</view>
+			</view>
+			<view class="uni-flex uni-row">
+				<view class="flex-item ">A</view>
+				<view class="flex-item ">B</view>
+				<view class="flex-item ">C</view>
+			</view>
+			<view class="uni-flex uni-row">
+				<view class="flex-item ">A</view>
+			</view>
+		</view>
+		<view class="yh_card">
+			<view class="index-title">数据统计</view>
+			<view class="uni-flex uni-row">
+				<view class="flex-item ">A</view>
+				<view class="flex-item ">B</view>
+				<view class="flex-item ">C</view>
+			</view>
+			<view class="uni-flex uni-row">
+				<view class="flex-item ">A</view>
+				<view class="flex-item ">B</view>
+				<view class="flex-item ">C</view>
+			</view>
+		
+		</view>
+		<view class="yh_card">
+			<view class="index-title">数据统计</view>
+			<view class="uni-flex uni-row">
+				<view class="flex-item ">A</view>
+				<view class="flex-item ">B</view>
+				<view class="flex-item ">C</view>
+			</view>
+			<view class="uni-flex uni-row">
+				<view class="flex-item ">A</view>
+				<view class="flex-item ">B</view>
+				<view class="flex-item ">C</view>
+			</view>
+			<view class="uni-flex uni-row">
+				<view class="flex-item ">A</view>
+				<view class="flex-item ">B</view>
+				<view class="flex-item ">C</view>
+			</view>
+			<view class="uni-flex uni-row">
+				<view class="flex-item ">A</view>
+				<view class="flex-item ">B</view>
+				<view class="flex-item ">C</view>
+			</view>
+			<view class="uni-flex uni-row">
+				<view class="flex-item ">A</view>
+			
+			</view>
+		</view>
+	
+       
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onNavigationBarButtonTap:function(e){
+			console.log('object');
+            console.log(JSON.stringify(e))
+        },
+		onLoad() {
+	
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+    .logo{
+        height: 200upx;
+        width: 200upx;
+        margin-top: 200upx;
+    }
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+	.index-title{text-align: left;padding:10upx 30upx}
+	.uni-flex{
+		flex-wrap: wrap;
+	}
+	.flex-item {
+		width: 33.3%;
+		text-align: center;
+	}
+</style>
